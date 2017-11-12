356c41eacd30079655394c5269494dd5b94ee92a
Bruno Schalch <brunoschalch@gmail.com>
2017-11-12 00:10:02 -0600

    File parsing

--- src/com/chomskygrammars/Main.java
+++ src/com/chomskygrammars/Main.java
@@ -4,30 +4,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
 
+    public static void main(String[] args) {
+
+
+        String[] nonTerminals=null;
+        String[] terminals = null;
+        String startingSymbol="";
+        String inputString="";
+        ArrayList<String[]> productions = new ArrayList<>();
+        String productionsUnparsed="";
+
+        String readLine = "";
         try {
 
-            File f = new File("src/com/mkyong/data.txt");
+            File f = new File("../../input/input.txt");
 
             BufferedReader b = new BufferedReader(new FileReader(f));
 
-            String readLine = "";
 
-            System.out.println("Reading file using Buffered Reader");
+
+            int i=0;
 
             while ((readLine = b.readLine()) != null) {
-                System.out.println(readLine);
+                i++;
+
+                if (i==1) {
+                    nonTerminals = readLine.split(",");
+                } else if(i==2) {
+                    terminals = readLine.split(",");
+                } else if (i==3) {
+                    startingSymbol = readLine;
+                } else if (i==4) {
+                    inputString = readLine;
+                } else if (i==5) {
+                    productionsUnparsed +=readLine;
+                } else {
+                    productionsUnparsed +="\n"+readLine;
+                }
+
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        Scanner scanner = new Scanner(productionsUnparsed);
+        while ((readLine = scanner.nextLine())!=null) {
+            productions.add(readLine.split("->"));
+        }
 
     }
 }
