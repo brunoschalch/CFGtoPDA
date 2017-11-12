d3e024c7948f3822c881b3f874103355804a85b5
Bruno Schalch <brunoschalch@gmail.com>
2017-11-11 23:47:54 -0600

    Setting up

--- src/com/chomskygrammars/Main.java
+++ src/com/chomskygrammars/Main.java
@@ -1,10 +1,33 @@
 package com.chomskygrammars;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+
+        try {
+
+            File f = new File("src/com/mkyong/data.txt");
+
+            BufferedReader b = new BufferedReader(new FileReader(f));
+
+            String readLine = "";
+
+            System.out.println("Reading file using Buffered Reader");
+
+            while ((readLine = b.readLine()) != null) {
+                System.out.println(readLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
